962651a009972c09dc4d6c5130229a8be1d55139
Beyhan <beyhan.taylan@hotmail.com>
2023-01-24 17:54:18 -0500

    question and answer posted

--- src/Personality.js
+++ src/Personality.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 function Personality({ questionList }) {
-
   console.log(questionList);
   const postQuestion = questionList.map((question) => {
     return (
       <div>
         <p>{question.question}</p>
-        {question.answers.map((answer) => {return(<p>{answer.answer}</p>)})}
+        {question.answers.map((answer) => {
+          return <p>{answer.answer}</p>;
+        })}
       </div>
     );
   });
