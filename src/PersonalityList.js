bdd0b0882a43b1e16ce2999c80470afaac57f2a8
Beyhan <btstructure97@gmail.com>
2023-01-25 19:59:50 -0500

    personalitylist

--- src/PersonalityList.js
+++ src/PersonalityList.js
@@ -1,24 +1,27 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
 function PersonalityList() {
-    const [personalityList, setPersonalityList] = useState([])
-    useEffect(() => {
-      fetch("http://localhost:3002/personality")
-        .then((res) => res.json())
-        .then((data) => setQuestionList(data));
-    }, []);
-  
-    const list = personalityList.map(list => {
-      return (
-        <div>
-          <p>{list.fourLetter}</p>
-          <p>{list.description}</p>
-        </div>
-      )
-    })
+  const [personalityList, setPersonalityList] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3002/personality")
+      .then((res) => res.json())
+      .then((data) => setQuestionList(data));
+  }, []);
+
+  const list = personalityList.map((list) => {
+    return (
+      <div>
+        <p>{list.fourLetter}</p>
+        <p>{list.description}</p>
+      </div>
+    );
+  });
   return (
-    <div>PersonalityList</div>
-  )
+    <div>
+      <p>Personality List</p>
+      <p>{list}</p>
+    </div>
+  );
 }
 
-export default PersonalityList
+export default PersonalityList;
